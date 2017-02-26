dc2b9583bc842c64c2f97773971fc8d3f974558e
Carly <cj.langlois.cl@gmail.com>
2017-02-25 21:13:37 -0600

    modal content changes

--- src/app/logged-in-home/logged-in-home.component.ts
+++ src/app/logged-in-home/logged-in-home.component.ts
@@ -49,62 +49,74 @@ export class LoggedInHomeComponent implements OnInit {
            {
                title: 'Trivia Night - Garden Style',
                start: '2017-02-18T18:00:00',
-               end: '2017-02-18T22:00:00'
+               end: '2017-02-18T22:00:00',
+               description: 'Lorem ipsum'
            },
            {
              title: 'Science and Sustainability Open House',
              start: '2017-02-25T13:00:00',
-             end: '2017-02-25T16:00:00'
+             end: '2017-02-25T16:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'Science and Sustainability Open House',
              start: '2017-02-26T13:00:00',
-             end: '2017-02-26T16:00:00'
+             end: '2017-02-26T16:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'Member Speaker Series: Gardening with Native Plants',
              start: '2017-03-07T11:00:00',
              end: '2017-03-07T12:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'Daffodil Dash 5k Trail Run',
              start: '2017-04-08T09:00:00',
-             end: '2017-04-08T10:00:00'
+             end: '2017-04-08T10:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'Eggstravaganze',
              start: '2017-04-08T10:00:00',
-             end: '2017-04-08T13:00:00'
+             end: '2017-04-08T13:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'Member Speaker Series: Best New Plants for the Home Landscape',
              start: '2017-04-18T11:00:00',
              end: '2017-04-18T12:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'Herb Days',
              start: '2017-04-27T17:00:00',
-             end: '2017-04-29T17:00:00'
+             end: '2017-04-29T17:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'Tulip Trot',
              start: '2017-04-30T07:00:00',
-             end: '2017-04-30T09:00:00'
+             end: '2017-04-30T09:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'St. Louis Garden Tour',
              start: '2017-06-11T09:30:00',
-             end: '2017-06-11T16:30:00'
+             end: '2017-06-11T16:30:00',
+             description: 'Lorem ipsum'
            },
            {
              title: 'Garden Gallop 5K',
              start: '2017-06-25T07:00:00',
-             end: '2017-06-25T09:00:00'
+             end: '2017-06-25T09:00:00',
+             description: 'Lorem ipsum'
            },
            {
              title: "Henry Shaw's Birthday Celebration!" ,
              start: '2017-07-24T09:00:00',
-             end: '2017-07-24T17:00:00'
+             end: '2017-07-24T17:00:00',
+             description: 'Lorem ipsum'
            }
        ],
        fixedWeekCount: false,
@@ -115,6 +127,8 @@ export class LoggedInHomeComponent implements OnInit {
 
        eventClick: function(event) {
 
+            $('.modal-title').html(event.title);
+            $('.modal-body').html(event.description);
             $(this).attr('data-toggle','modal');
             $(this).attr('data-target', '#myModal');
             //$(this).css('background-color', 'red');
